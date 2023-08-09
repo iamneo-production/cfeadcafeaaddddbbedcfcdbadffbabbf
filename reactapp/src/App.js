b84f3af02bcdacc88f3449fc94422d8e9636abac
Iamneo <techadmin@iamneo.ai>
2023-08-09 04:26:22 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,62 +1,14 @@
-import React, { useState, useEffect} from 'react';
-import "./Stopwatch.css"
+import Stopwatch from './components/Stopwatch/Stopwatch';
+import './App.css';
 
-const Stopwatch = () => {
-  const [time, setTime] = useState(0);
-  const [isRunning, setIsRunning] = useState(false);
-  const [startTime, setStartTime] = useState(null);
-  const [pausedTime, setPausedTime] = useState(null);
-
-  useEffect(() => {
-    let intervalId;
-    if (isRunning) {
-      intervalId = setInterval(() => {
-        setTime((Date.now() - startTime) + (pausedTime || 0));
-      }, 1000);
-    }
-    return () => clearInterval(intervalId);
-  }, [isRunning, startTime, pausedTime]);
-
-  const handleStart = () => {
-    setStartTime(Date.now());
-    setPausedTime(null);
-    setIsRunning(true);
-  };
-
-  const handlePause = () => {
-    setPausedTime(time);
-    setIsRunning(false);
-  };
-
-  const handleResume = () => {
-    setStartTime(Date.now());
-    setIsRunning(true);
-  };
-
-  const handleReset = () => {
-    setIsRunning(false);
-    setStartTime(null);
-    setPausedTime(null);
-    setTime(0);
-  };
-
-  const formattedTime = new Date(time).toISOString().substr(11, 8);
-
+function App() {
   return (
-    <div>
-        <h1>React Stopwatch</h1>
-      <p data-testid="time">{formattedTime}</p>
-      <div className="buttons">
-      {isRunning ? 
-        <button data-testid="pause" onClick={handlePause}>Pause</button> :
-        startTime ? 
-          <button data-testid="resume" onClick={handleResume}>Resume</button> : 
-          <button data-testid="start" onClick={handleStart}>Start</button>
-      }
-      <button data-testid="reset" onClick={handleReset} disabled={!startTime}>Reset</button>
-      </div>
+    <div className ="container">
+    <div className="App">
+      <Stopwatch />
+    </div>
     </div>
   );
 }
 
-export default Stopwatch;
+export default App;
